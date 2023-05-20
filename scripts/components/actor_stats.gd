e6a879e1eca7ac2c44474a05a34bae14d9fd3f00
Snayff <snaith2@gmail.com>
2023-05-20 09:41:01 +0100

    added stamina stat

--- scripts/components/actor_stats.gd
+++ scripts/components/actor_stats.gd
@@ -1,100 +1,6 @@
 class_name ActorStats extends Resource
 
-# signals
-## Emitted when a character has no `health` left.
-signal health_depleted
-## Emitted every time the value of `health` changes.
-signal health_changed(old_value, new_value)
-
-var _modifiers := {}
-
-# defence stats
-@export_group("Defence")
-@export var max_health: int
-@export var health: int:
-	set(value):
-		var previous_health := health
-		health = clamp(value, 0, max_health)
-		emit_signal("health_changed", previous_health, health)
-
-		# inform of death
-		if health == 0:
-			emit_signal("health_depleted")
-@export var base_regen: int:
-	set(value):
-		base_regen = value
-		_recalculate("regen")
-@export var regen: int
-@export var base_dodge: int:
-	set(value):
-		base_dodge = value
-		_recalculate("dodge")
-@export var dodge: int
-@export var base_magic_defence: int:
-	set(value):
-		base_magic_defence = value
-		_recalculate("magic_defence")
-@export var magic_defence: int
-@export var base_mundane_defence: int:
-	set(value):
-		base_mundane_defence = value
-		_recalculate("mundane_defense")
-@export var mundane_defence: int
-
-# attack stats
-@export_group("Offence")
-@export var base_attack: int:
-	set(value):
-		base_attack = value
-		_recalculate("attack")
-@export var attack: int
-@export var base_attack_speed: float:
-	set(value):
-		base_attack_speed = value
-		_recalculate("attack_speed")
-@export var attack_speed: float
-@export var base_crit_chance: int:
-	set(value):
-		base_crit_chance = value
-		_recalculate("crit_chance")
-@export var crit_chance: int
-@export var damage_type: Constants.DamageType
-@export var base_penetration: int:
-	set(value):
-		base_penetration = value
-		_recalculate("penetration")
-@export var penetration: int
-
-# misc stats
-@export_group("Misc Combat")
-@export var base_attack_range: int:
-	set(value):
-		base_attack_range = value
-		_recalculate("attack_range")
-@export var attack_range: int
-@export var base_move_speed: int:
-	set(value):
-		base_move_speed = value
-		_recalculate("move_speed")
-@export var move_speed: int
-@export var base_count: int:
-	set(value):
-		base_count = value
-		_recalculate("num_units")
-@export var num_units: int
-
-# non-combat
-@export_group("Non Combat")
-@export var base_gold_cost: int:
-	set(value):
-		base_gold_cost = value
-		_recalculate("gold_cost")
-@export var gold_cost: int
-@export var tier: int
-@export var faction: String
-
-
-const MODIFIABLE_STATS = [
+const MODIFIABLE_STATS : Array[String] = [
 	"max_health",
 	"regen",
 	"dodge",
@@ -109,6 +15,110 @@ const MODIFIABLE_STATS = [
 	"count"
 
 ]
+
+####### SIGNALS #########
+
+## Emitted when a character has no `health` left.
+signal health_depleted
+## Emitted every time the value of `health` changes.
+signal health_changed(old_value, new_value)
+
+######### ATTRIBUTES ###########
+
+var _modifiers : Dictionary = {}
+
+# defence stats
+@export_group("Defence")
+@export var max_health : int
+@export var health : int:
+	set(value):
+		var previous_health := health
+		health = clamp(value, 0, max_health)
+		emit_signal("health_changed", previous_health, health)
+
+		# inform of death
+		if health == 0:
+			emit_signal("health_depleted")
+@export var base_regen : int:
+	set(value):
+		base_regen = value
+		_recalculate("regen")
+@export var regen : int
+@export var base_dodge : int:
+	set(value):
+		base_dodge = value
+		_recalculate("dodge")
+@export var dodge : int
+@export var base_magic_defence : int:
+	set(value):
+		base_magic_defence = value
+		_recalculate("magic_defence")
+@export var magic_defence : int
+@export var base_mundane_defence : int:
+	set(value):
+		base_mundane_defence = value
+		_recalculate("mundane_defense")
+@export var mundane_defence : int
+
+# attack stats
+@export_group("Offence")
+@export var base_attack : int:
+	set(value):
+		base_attack = value
+		_recalculate("attack")
+@export var attack : int
+@export var base_attack_speed : float:
+	set(value):
+		base_attack_speed = value
+		_recalculate("attack_speed")
+@export var attack_speed : float
+@export var base_crit_chance : int:
+	set(value):
+		base_crit_chance = value
+		_recalculate("crit_chance")
+@export var crit_chance : int
+@export var damage_type : Constants.DamageType
+@export var base_penetration : int:
+	set(value):
+		base_penetration = value
+		_recalculate("penetration")
+@export var penetration : int
+
+# misc stats
+@export_group("Misc Combat")
+@export var base_attack_range : int:
+	set(value):
+		base_attack_range = value
+		_recalculate("attack_range")
+@export var attack_range : int
+@export var base_move_speed : int:
+	set(value):
+		base_move_speed = value
+		_recalculate("move_speed")
+@export var move_speed : int
+@export var base_num_units : int:
+	set(value):
+		base_num_units = value
+		_recalculate("num_units")
+@export var num_units : int
+@export var base_stamina : int :
+	set(value):
+		base_stamina = value
+		_recalculate("stamina")
+@export var stamina : int
+
+
+
+# non-combat
+@export_group("Non Combat")
+@export var base_gold_cost : int:
+	set(value):
+		base_gold_cost = value
+		_recalculate("gold_cost")
+@export var gold_cost : int
+@export var tier : int
+@export var faction : String
+
 
 func _init() -> void:
 	# create dict for each state to hold stat modifiers
