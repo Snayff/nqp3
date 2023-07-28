72f18800d580f7855d1b13bd9daa7f9ff4404351
Daniel Queiroz Porto <danielvandalk@gmail.com>
2023-07-28 16:35:37 -0300

    hotfix/node_names_and_signal_checks (#33)

--- scenes/entities/actor/actor_debug_info.gd
+++ scenes/entities/actor/actor_debug_info.gd
@@ -5,4 +5,7 @@ func _process(delta: float) -> void:
 	var state = Constants.ActorState.keys()[parent._state]
 	var anim = parent.animated_sprite.animation
 	var target = parent._target
-	set_text("State: " + str(state) + "\n Anim: " + anim + "\n Target: "  + str(target.uid))
+	if target == null:
+		set_text("State: " + str(state) + "\n Anim: " + anim + "\n Target: ")
+	else:
+		set_text("State: " + str(state) + "\n Anim: " + anim + "\n Target: "  + str(target.uid))
