26322cd6015cc3e191a645f2df6d3f2b9ce60890
Daniel Queiroz Porto <danielvandalk@gmail.com>
2023-08-09 04:13:12 -0300

    feature/ref_data_to_use_refcounted (#39)
    * refactor ref_data to use RefCounted
    
    * Fix _create_actor_stats
    
    The "automation" I tried to do doesn't work because almost no property
    in ActorStats has the same name as in UnitData

---
+++ scripts/data classes/unit_data.gd
@@ -0,0 +1,26 @@
+class_name UnitData 
+extends RefCounted
+
+var max_health := 100
+var max_stamina := 100
+var regen := 100
+var dodge := 100
+var magic_defence := 10
+var mundane_defence := 10
+var attack := 50
+var attack_speed := 100
+var penetration := 100
+var crit_chance := 100
+var move_speed := 150
+var stamina := 10
+var num_units := 5
+var faction := "faction1"
+var gold_cost := 100
+var tier := 1
+var path_base_sprites := Constants.PATH_SPRITES_ACTORS
+var actions := {}
+
+func _init(overrides := {}):
+	for key in overrides:
+		if key in self:
+			set(key, overrides[key])
--- scripts/globals/ref_data.gd
+++ scripts/globals/ref_data.gd
@@ -3,138 +3,82 @@ extends Node
 ##
 ## Read Only.
 
-const unit_data: Dictionary = {
-	"copper_golem": {
-		"max_health": 100,
-		"max_stamina": 100,
-		"regen": 100,
-		"dodge": 100,
-		"magic_defence": 10,
-		"mundane_defence": 10,
-		"attack": 50,
-		"attack_speed": 100,
-		"penetration": 100,
-		"crit_chance": 100,
-		"move_speed": 150,
-		"stamina": 10,
-		"num_units": 5,
-		"faction": "faction1",
-		"gold_cost": 100,
-		"tier": 1,
-		"actions": {  ## must use {Action Type, script name} (NOT class name)
-			Constants.ActionType.ATTACK : [
-				"smash",
-			],
-			Constants.ActionType.REACTION : {
-				Constants.ActionTrigger.ON_DEAL_DAMAGE : [
-					"spiky_shell",
-				]
-			},
-		},
-		"path_base_sprites": Constants.PATH_SPRITES_ACTORS,
-	},
-	"conjurer": {
-		"max_health": 70,
-		"max_stamina": 100,
-		"regen": 100,
-		"dodge": 100,
-		"magic_defence": 2,
-		"mundane_defence": 3,
-		"attack": 33,
-		"attack_speed": 100,
-		"penetration": 100,
-		"crit_chance": 100,
-		"move_speed": 200,
-		"stamina": 10,
-		"num_units": 3,
-		"faction": "faction1",
-		"gold_cost": 100,
-		"tier": 1,
-		"actions": {  ## must use Action Type, script name (NOT class name)
-			Constants.ActionType.ATTACK : [
-				"wand_blast",
-				"heal",
-			],
-			Constants.ActionType.REACTION : { }
-		},
-		"path_base_sprites": Constants.PATH_SPRITES_ACTORS,
-	},
-	"poet": {
-		"max_health": 70,
-		"max_stamina": 100,
-		"regen": 100,
-		"dodge": 100,
-		"magic_defence": 2,
-		"mundane_defence": 3,
-		"attack": 33,
-		"attack_speed": 100,
-		"penetration": 100,
-		"crit_chance": 100,
-		"move_speed": 200,
-		"stamina": 10,
-		"num_units": 2,
-		"faction": "faction1",
-		"gold_cost": 100,
-		"tier": 1,
-		"actions": {  ## must use Action Type, script name (NOT class name)
-			Constants.ActionType.ATTACK : [
-				"stanza",
-			],
-			Constants.ActionType.REACTION : { }
-		},
-		"path_base_sprites": Constants.PATH_SPRITES_ACTORS,
-	},
-	"cavalier": {
-		"max_health": 100,
-		"max_stamina": 100,
-		"regen": 100,
-		"dodge": 100,
-		"magic_defence": 10,
-		"mundane_defence": 10,
-		"attack": 50,
-		"attack_speed": 100,
-		"penetration": 100,
-		"crit_chance": 100,
-		"move_speed": 150,
-		"stamina": 10,
-		"num_units": 1,
-		"faction": "faction1",
-		"gold_cost": 100,
-		"tier": 1,
-		"actions": {  ## must use {Action Type, script name} (NOT class name)
-			Constants.ActionType.ATTACK : [
-				"smash",
-				"wand_blast",
-			],
-			Constants.ActionType.REACTION : { },
-		},
-		"path_base_sprites": Constants.PATH_SPRITES_COMMANDERS,
-	},
-	"knight": {
-		"max_health": 100,
-		"max_stamina": 100,
-		"regen": 100,
-		"dodge": 100,
-		"magic_defence": 10,
-		"mundane_defence": 10,
-		"attack": 50,
-		"attack_speed": 100,
-		"penetration": 100,
-		"crit_chance": 100,
-		"move_speed": 150,
-		"stamina": 10,
-		"num_units": 1,
-		"faction": "faction1",
-		"gold_cost": 100,
-		"tier": 1,
-		"actions": {  ## must use {Action Type, script name} (NOT class name)
-			Constants.ActionType.ATTACK : [
-				"smash",
-				"wand_blast",
-			],
-			Constants.ActionType.REACTION : { },
-		},
-		"path_base_sprites": Constants.PATH_SPRITES_COMMANDERS,
-	},
-
-}
+func get_unit_data(unit_name: String) -> UnitData:
+	var value: UnitData = null
+	
+	match unit_name:
+		"copper_golem":
+			var actions = {  ## must use {Action Type, script name} (NOT class name)
+				Constants.ActionType.ATTACK : [
+					"smash",
+				],
+				Constants.ActionType.REACTION : {
+					Constants.ActionTrigger.ON_DEAL_DAMAGE : [
+						"spiky_shell",
+					]
+				}
+			}
+			value = UnitData.new({
+				"actions": actions,
+			})
+		"conjurer":
+			var actions := {  ## must use Action Type, script name (NOT class name)
+				Constants.ActionType.ATTACK : [
+					"wand_blast",
+					"heal",
+				],
+				Constants.ActionType.REACTION : { }
+			}
+			value = UnitData.new({
+				"max_health": 70,
+				"magic_defence": 2,
+				"mundane_defence": 3,
+				"attack": 33,
+				"move_speed": 200,
+				"num_units": 3,
+				"actions": actions,
+			})
+		"poet":
+			var actions := {  ## must use Action Type, script name (NOT class name)
+				Constants.ActionType.ATTACK : [
+					"stanza",
+				],
+				Constants.ActionType.REACTION : { }
+			}
+			value = UnitData.new({
+				"max_health": 70,
+				"magic_defence": 2,
+				"mundane_defence": 3,
+				"attack": 33,
+				"move_speed": 200,
+				"num_units": 2,
+				"actions": actions,
+			})
+		"cavalier":
+			var actions := {  ## must use {Action Type, script name} (NOT class name)
+				Constants.ActionType.ATTACK : [
+					"smash",
+					"wand_blast",
+				],
+				Constants.ActionType.REACTION : { },
+			}
+			value = UnitData.new({
+				"num_units": 1,
+				"actions": actions,
+				"path_base_sprites": Constants.PATH_SPRITES_COMMANDERS
+			})
+		"knight":
+			var actions := {  ## must use {Action Type, script name} (NOT class name)
+				Constants.ActionType.ATTACK : [
+					"smash",
+					"wand_blast",
+				],
+				Constants.ActionType.REACTION : { },
+			}
+			value = UnitData.new({
+				"num_units": 1,
+				"actions": actions,
+				"path_base_sprites": Constants.PATH_SPRITES_COMMANDERS
+			})
+	
+	return value
